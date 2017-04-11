046f1e1a31d06787d8156c4da0885e37acdc47e8
Chris <chris@victini.Princeton.EDU>
2017-04-11 17:00:15 -0400

    asdf

--- src/gpt.rs
+++ src/gpt.rs
@@ -12,28 +12,28 @@ pub struct Header
 {
 	pub signature: String, // EFI PART
 	pub revision: u32, // 00 00 01 00
-	pub header_size_le: [u8;4], // little endian
-	pub crc32: [u8;4], 
-	pub reserved: [u8;4], // must be 0
-	pub current_lba: [u8; 8],
-	pub backup_lba: [u8; 8],
-	pub first_usable: [u8; 8],
-	pub last_usable: [u8; 8],
+	pub header_size_le: u32, // little endian
+	pub crc32: u32, 
+	pub reserved: u32, // must be 0
+	pub current_lba: u64,
+	pub backup_lba: u64,
+	pub first_usable: u64,
+	pub last_usable: u64,
 	pub disk_guid: uuid::Uuid,
-	pub start_lba: [u8; 8],
-	pub num_parts: [u8; 4],
-	pub part_size: [u8; 4], // usually 128
-	pub crc32_parts: [u8; 4]
+	pub start_lba: u64,
+	pub num_parts: u32,
+	pub part_size: u32, // usually 128
+	pub crc32_parts: u32
 }
 
-fn parse_uuid(bytes: &[u8]) -> Result<Uuid, Error>
+fn parse_uuid(rdr: &Cursor<[u8; 92]>) -> Result<Uuid, Error>
 {
-	let mut rdr = Cursor::new(bytes);
+	//let mut rdr = Cursor::new(bytes);
 	let d1: u32 = rdr.read_u32::<LittleEndian>().unwrap();
 	let d2: u16 = rdr.read_u16::<LittleEndian>().unwrap();
 	let d3: u16 = rdr.read_u16::<LittleEndian>().unwrap();
 
-	match Uuid::from_fields(d1, d2, d3, &bytes[8..])
+	match Uuid::from_fields(d1, d2, d3, rdr.read_u32::<BigEndian>())
 	{
 		Ok(uuid) => Ok(uuid),
 		Err(_) => Err(Error::new(ErrorKind::Other, "Invalid Disk UUID?"))
@@ -56,33 +56,31 @@ pub fn read_header2(path:&String) -> Result<Header, Error>
 
 	let mut hdr: [u8; 92] = [0; 92];
 
-	//let _ = file.read_exact(&mut hdr);
-	let mut reader = Cursor::new(file);
+	let _ = file.read_exact(&mut hdr);
+	let mut reader = Cursor::new(&mut hdr[..]);
 
-	let sigstr: String = String::from_utf8_lossy(reader.read_u64::<LittleEndian>());
+	let sigstr = reader.read_u64::<LittleEndian>()?.to_string();
 
-	if sigstr.as_ref() != "EFI PART" 
+	if sigstr != "EFI PART" 
 	{
 		return Err(Error::new(ErrorKind::Other, "Invalid GPT Signature."))
 	};
 
-
-	let guid: uuid::Uuid = parse_uuid(&hdr[56..72])?;
 	let h = Header{
-		signature: sigstr.to_string(), //clone_into_array(&hdr[0..8]), 
-		revision: &hdr[8..12], 
-		header_size_le: clone_into_array(&hdr[12..16]), 
-		crc32: clone_into_array(&hdr[16..20]), 
-		reserved: clone_into_array(&hdr[20..24]),
-		current_lba: clone_into_array(&hdr[24..32]),
-		backup_lba: clone_into_array(&hdr[32..40]),
-		first_usable: clone_into_array(&hdr[40..48]),
-		last_usable: clone_into_array(&hdr[48..56]),
-		disk_guid: guid,
-		start_lba: clone_into_array(&hdr[72..80]),
-		num_parts: clone_into_array(&hdr[80..84]),
-		part_size: clone_into_array(&hdr[84..88]),
-		crc32_parts: clone_into_array(&hdr[88..])
+		signature:      sigstr.to_string(),
+		revision:       reader.read_u32::<LittleEndian>().unwrap(), 
+		header_size_le: reader.read_u32::<LittleEndian>().unwrap(), 
+		crc32:          reader.read_u32::<LittleEndian>().unwrap(), 
+		reserved:       reader.read_u32::<LittleEndian>().unwrap(),
+		current_lba:    reader.read_u64::<LittleEndian>().unwrap(), 
+		backup_lba:     reader.read_u64::<LittleEndian>().unwrap(), 
+		first_usable:   reader.read_u64::<LittleEndian>().unwrap(), 
+		last_usable:    reader.read_u64::<LittleEndian>().unwrap(), 
+		disk_guid:      parse_uuid(&mut reader)?,
+		start_lba:      reader.read_u64::<LittleEndian>().unwrap(),
+		num_parts:      reader.read_u32::<LittleEndian>().unwrap(),
+		part_size:      reader.read_u32::<LittleEndian>().unwrap(),
+		crc32_parts:    reader.read_u32::<LittleEndian>().unwrap()
 	};
 
 	Ok(h)
