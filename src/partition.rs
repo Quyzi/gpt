49f4c16c38ac52b04f141526079715fa0db06613
Chris Holcombe <xfactor973@gmail.com>
2017-04-13 19:50:16 -0700

    Fix up json and parser

--- src/partition.rs
+++ src/partition.rs
@@ -15,72 +15,78 @@ use self::crc::crc32;
 
 #[derive(Debug)]
 pub struct Partition {
-	part_type_guid: PartitionType,
-	part_guid: uuid::Uuid,
-	first_LBA: u32, // little endian
-	last_LBA: u32,
-	flags: u32,
-	name: String // 36 UTF-16LE code units
+    part_type_guid: PartitionType,
+    part_guid: uuid::Uuid,
+    first_LBA: u32, // little endian
+    last_LBA: u32,
+    flags: u32,
+    name: String, // 36 UTF-16LE code units
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct PartitionType {
-	os: String,
-	guid: String,
-	desc: String
+    os: String,
+    guid: String,
+    desc: String,
 }
 
 fn read_part_name(rdr: &mut Cursor<&[u8]>) -> String {
-	let mut namebytes: Vec<u16> = Vec::new();
-	for _ in 0..36 {
-		let b = rdr.read_u16::<LittleEndian>().unwrap();
-		if b != 0 {
-			namebytes.push(b);
-		}
-	}
+    let mut namebytes: Vec<u16> = Vec::new();
+    for _ in 0..36 {
+        let b = rdr.read_u16::<LittleEndian>().unwrap();
+        if b != 0 {
+            namebytes.push(b);
+        }
+    }
 
-	return String::from_utf16_lossy(&namebytes);
+    return String::from_utf16_lossy(&namebytes);
 }
 
 fn parse_parttype_guid(str: uuid::Uuid) -> Result<PartitionType, Error> {
-	let uuid = str.hyphenated().to_string().to_uppercase();
-	let mut file = File::open("types.json")?;
-	let mut json: String = String::new();
-	let _ = file.read_to_string(&mut json);
-	let mut guids: Vec<PartitionType> = serde_json::from_str(&json)?;
+    let uuid = str.hyphenated().to_string().to_uppercase();
+    let mut file = File::open("types.json")?;
+    let mut json: String = String::new();
+    let _ = file.read_to_string(&mut json);
+    let mut guids: Vec<PartitionType> =
+        serde_json::from_str(&json).map_err(|e: serde_json::Error|
+			Error::new(ErrorKind::Other, e.to_string()))?;
 
-	Ok(PartitionType {os: String::from("Dummy"), desc: String::from("Dummy"), guid: uuid})
+    Ok(PartitionType {
+           os: String::from("Dummy"),
+           desc: String::from("Dummy"),
+           guid: uuid,
+       })
 
 }
 
 pub fn read_partitions(path: &String, header: &Header) -> Result<Vec<Partition>, Error> {
-	let mut file = File::open(path)?;
+    let mut file = File::open(path)?;
     let _ = file.seek(SeekFrom::Start(512 * header.part_start));
 
     let mut parts: Vec<Partition> = Vec::new();
 
     for _ in 0..header.num_parts {
-    	let mut bytes: [u8; 56] = [0; 56];
-    	let mut nameraw: [u8; 72] = [0; 72]; 
+        let mut bytes: [u8; 56] = [0; 56];
+        let mut nameraw: [u8; 72] = [0; 72];
 
-    	let _ = file.read_exact(&mut bytes);
-    	let _ = file.read_exact(&mut nameraw);
-    	let partname = read_part_name(&mut Cursor::new(&nameraw[..]));
+        let _ = file.read_exact(&mut bytes);
+        let _ = file.read_exact(&mut nameraw);
+        let partname = read_part_name(&mut Cursor::new(&nameraw[..]));
 
-    	let mut reader = Cursor::new(&bytes[..]);
+        let mut reader = Cursor::new(&bytes[..]);
 
-    	let p: Partition = Partition {
-    		part_type_guid: parse_parttype_guid(parse_uuid(&mut reader)?)?,
-    		part_guid: parse_uuid(&mut reader)?,
-    		first_LBA: reader.read_u32::<LittleEndian>()?,
-    		last_LBA: reader.read_u32::<LittleEndian>()?,
-    		flags: reader.read_u32::<LittleEndian>()?,
-    		name: partname.to_string()
-    	};
+        let p: Partition = Partition {
+            part_type_guid: parse_parttype_guid(parse_uuid(&mut reader)?)?,
+            part_guid: parse_uuid(&mut reader)?,
+            first_LBA: reader.read_u32::<LittleEndian>()?,
+            last_LBA: reader.read_u32::<LittleEndian>()?,
+            flags: reader.read_u32::<LittleEndian>()?,
+            name: partname.to_string(),
+        };
 
-    	if p.part_guid.simple().to_string() != "00000000000000000000000000000000" {
-    		parts.push(p);
-    	}
+        if p.part_guid.simple().to_string() != "00000000000000000000000000000000" {
+            parts.push(p);
+        }
     }
 
     Ok(parts)
