fba52bbc1f4568385f5aed4e70e8592ef74c5399
Chris Holcombe <xfactor973@gmail.com>
2017-09-29 14:19:12 -0700

    Rustfmt partition.rs and add tracing information

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #[macro_use]
+extern crate log;
+#[macro_use]
 extern crate serde_derive;
 
 pub mod header;
--- src/partition.rs
+++ src/partition.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Read, Seek, Cursor, SeekFrom, Error, ErrorKind};
+use std::io::{Read, Seek, Cursor, SeekFrom, Error, ErrorKind, Result};
 use std::fmt;
 
 use header::{Header, parse_uuid};
@@ -7,8 +7,6 @@ use header::{Header, parse_uuid};
 extern crate uuid;
 extern crate byteorder;
 extern crate crc;
-#[macro_use]
-extern crate log;
 extern crate serde;
 extern crate serde_json;
 
@@ -40,38 +38,54 @@ struct PartitionType {
 
 impl fmt::Display for Partition {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Partition:\t\t{}\nPartition GUID:\t\t{}\nPartition Type:\t\t{}\t{}\nSpan:\t\t\t{} - {}\nFlags:\t\t\t{}", self.name, self.part_guid, self.part_type_guid.guid, self.part_type_guid.desc, self.first_LBA, self.last_LBA, self.flags)
+        write!(
+            f,
+            "Partition:\t\t{}\nPartition GUID:\t\t{}\nPartition Type:\t\t{}\t{}\n\
+            Span:\t\t\t{} - {}\nFlags:\t\t\t{}",
+            self.name,
+            self.part_guid,
+            self.part_type_guid.guid,
+            self.part_type_guid.desc,
+            self.first_LBA,
+            self.last_LBA,
+            self.flags
+        )
     }
 }
 
-fn read_part_name(rdr: &mut Cursor<&[u8]>) -> String {
+fn read_part_name(rdr: &mut Cursor<&[u8]>) -> Result<String> {
+    trace!("Reading partition name from {:?}", rdr);
     let mut namebytes: Vec<u16> = Vec::new();
     for _ in 0..36 {
-        let b = rdr.read_u16::<LittleEndian>().unwrap();
+        let b = rdr.read_u16::<LittleEndian>()?;
         if b != 0 {
             namebytes.push(b);
         }
     }
 
-    return String::from_utf16_lossy(&namebytes);
+    return Ok(String::from_utf16_lossy(&namebytes));
 }
 
-fn parse_parttype_guid(str: uuid::Uuid) -> Result<PartitionType, Error> {
+fn parse_parttype_guid(str: uuid::Uuid) -> Result<PartitionType> {
+    debug!("Parsing partition guid");
     let uuid = str.hyphenated().to_string().to_uppercase();
+    debug!("opening types.json to identify partition type");
     let mut file = File::open("types.json")?;
     let mut json: String = String::new();
     let _ = file.read_to_string(&mut json);
-    let mut guids: Vec<PartitionType> =
-        serde_json::from_str(&json).map_err(|e: serde_json::Error|
-			Error::new(ErrorKind::Other, e.to_string()))?;
+    let mut guids: Vec<PartitionType> = serde_json::from_str(&json).map_err(
+        |e: serde_json::Error| {
+            Error::new(ErrorKind::Other, e.to_string())
+        },
+    )?;
 
     for guid in guids {
-        if guid.guid  == uuid {
+        if guid.guid == uuid {
             return Ok(PartitionType {
                 guid: guid.guid,
                 os: guid.os,
-                desc: guid.desc
-            })
+                desc: guid.desc,
+            });
         }
     }
 
@@ -84,19 +98,21 @@ fn parse_parttype_guid(str: uuid::Uuid) -> Result<PartitionType, Error> {
 /// let header = read_header("/dev/sda").unwrap();
 /// let partitions: Vec<Partition> = read_partitions("/dev/sda", &mut header);
 ///
-pub fn read_partitions(path: &String, header: &Header) -> Result<Vec<Partition>, Error> {
-
+pub fn read_partitions(path: &String, header: &Header) -> Result<Vec<Partition>> {
+    debug!("reading partitions from file: {}", path);
     let mut file = File::open(path)?;
+    trace!("Seeking to {}", 512 * header.part_start);
     let _ = file.seek(SeekFrom::Start(512 * header.part_start));
     let mut parts: Vec<Partition> = Vec::new();
 
+    debug!("Reading partitions");
     for _ in 0..header.num_parts {
         let mut bytes: [u8; 56] = [0; 56];
         let mut nameraw: [u8; 72] = [0; 72];
 
         let _ = file.read_exact(&mut bytes);
         let _ = file.read_exact(&mut nameraw);
-        let partname = read_part_name(&mut Cursor::new(&nameraw[..]));
+        let partname = read_part_name(&mut Cursor::new(&nameraw[..]))?;
 
         let mut reader = Cursor::new(&bytes[..]);
 
@@ -114,12 +130,14 @@ pub fn read_partitions(path: &String, header: &Header) -> Result<Vec<Partition>,
         }
     }
 
+    trace!("Seeking to {}", 512 * header.part_start);
     let _ = file.seek(SeekFrom::Start(512 * header.part_start));
     let mut table: [u8; 16384] = [0; 16384];
     let _ = file.read_exact(&mut table);
 
+    debug!("Checking checksum");
     if crc32::checksum_ieee(&table) != header.crc32_parts {
-        return Err(Error::new(ErrorKind::Other, "Invalid partition table CRC."))
+        return Err(Error::new(ErrorKind::Other, "Invalid partition table CRC."));
     }
 
     Ok(parts)
