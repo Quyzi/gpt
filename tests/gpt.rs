0b146ea9196ee278a1a8100383908ce8e20b794b
Luca Bruno <luca.bruno@coreos.com>
2018-08-03 14:13:31 +0000

    tests/gpt: add a simple positive testcase

--- tests/gpt.rs
+++ tests/gpt.rs
@@ -25,3 +25,30 @@ fn test_gptconfig_empty() {
     assert_eq!(disk.backup_header(), None);
     assert!(disk.partitions().is_empty());
 }
+
+#[test]
+fn test_gptdisk_linux_01() {
+    let diskpath = path::Path::new("tests/fixtures/gpt-linux-disk-01.img");
+    let lb_size = disk::LogicalBlockSize::Lb512;
+
+    let disk = gpt::GptConfig::new()
+        .open(diskpath)
+        .unwrap();
+    assert_eq!(*disk.logical_block_size(), lb_size);
+    assert!(disk.primary_header().is_some());
+    assert!(disk.backup_header().is_some());
+    assert_eq!(disk.partitions().len(), 1);
+
+    let h1 = disk.primary_header().unwrap();
+    assert_eq!(h1.current_lba, 1);
+    assert_eq!(h1.backup_lba, 95);
+
+    let h2 = disk.backup_header().unwrap();
+    assert_eq!(h2.current_lba, 95);
+    assert_eq!(h2.backup_lba, 1);
+
+
+    let p1 = &disk.partitions()[0];
+    assert_eq!(p1.name, "primary");
+    assert_eq!(p1.part_type_guid.desc, "Linux Filesystem Data");
+}
